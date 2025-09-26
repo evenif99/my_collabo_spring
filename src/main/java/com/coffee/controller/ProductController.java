85412d53397076edf0bddcd9c8c57793fe8eaca8
evenif99 <back86538774@gmail.com>
2025-09-26 16:27:19 +0900

    spring collabo 09/26

--- src/main/java/com/coffee/controller/ProductController.java
+++ src/main/java/com/coffee/controller/ProductController.java
@@ -180,4 +180,16 @@ public class ProductController {
             return base64Image;
         }
     }
+
+    @GetMapping("/detail/{id}") // 프론트 엔드가 상품에 대한 상세 정보를 요청하였습니다.
+    public ResponseEntity<Product> detail(@PathVariable Long id){
+        Product product = this.productService.getProductById(id);
+
+        if(product == null){ // 404 NOT_FOUND 응답
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        }else { // 200 ok 응답
+            return ResponseEntity.ok(product);
+        }
+    }
 }
